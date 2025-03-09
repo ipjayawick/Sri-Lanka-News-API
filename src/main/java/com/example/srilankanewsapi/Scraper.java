3cb24ed6a3bcaf774a7c66609d85470b40d901dd
ipjayawick <ipjayawick@gmail.com>
2025-03-09 14:01:19 +0530

    image urls added

--- src/main/java/com/example/srilankanewsapi/Scraper.java
+++ src/main/java/com/example/srilankanewsapi/Scraper.java
@@ -5,6 +5,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +29,7 @@ public class Scraper {
                 for (Element postDiv : postDivs) {
                     // Extract the first <a> tag's href attribute
                     String source = postDiv.selectFirst("a").attr("href");
+                    String imageUrl= postDiv.selectFirst("a").selectFirst("img").attr("src");
 
                     // Extract the topic and description
                     Element col7Div = postDiv.selectFirst(".col-7");
@@ -37,7 +39,7 @@ public class Scraper {
                         String description = h5Tags.last() != null ? h5Tags.last().text().trim() : "";
 
                         // Store the extracted data in the list
-                        news.add(new News(topic, description,source));
+                        news.add(new News(topic, description,source,imageUrl));
                     }
                 }
             }
@@ -56,17 +58,22 @@ public class Scraper {
             // Select all elements containing news dates
             Elements dateElements = document.select(".promo-timestamp.bbc-11oryzm.e1mklfmt0");
 
+            //Select all elements containing images
+            Elements imageElements = document.select(".bbc-139onq");
+
             // Extract data
             int size = Math.min(titleElements.size(), dateElements.size()); // Ensure safe indexing
             for (int i = 0; i < size; i++) {
                 Element titleElement = titleElements.get(i);
                 Element dateElement = dateElements.get(i);
+                Element imageElement=imageElements.get(i);
 
                 String title = titleElement.text();
                 String source = titleElement.selectFirst("a") != null ? titleElement.selectFirst("a").attr("href") : "#";
                 String date = dateElement.text();
+                String imageUrl=imageElement.attr("src");
 
-                news.add(new News(title,"",source));
+                news.add(new News(title,"",source,imageUrl));
             }
         } catch (IOException e) {
             e.printStackTrace();
